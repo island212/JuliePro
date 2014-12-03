c23e8855be7e4924d380a06752a5c920d4b7f91c
ModoNoob <ModoNoob@hotmail.fr>
2014-12-03 15:20:33 -0500

    Changes to login.php

--- login.php
+++ login.php
@@ -1,3 +1,11 @@
 <?php
+    if(isset($_POST['username']) && isset($_POST['password'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+    }
+    else {
+        include('errors/login_failed.php');
+    }
 
+    
 ?>
