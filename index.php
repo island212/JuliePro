3340cf9b22f4306c16c9eb09ea31c4fa77286e2c
Island <islandx424@gmail.com>
2014-12-03 15:24:09 -0500

    Recreate index.php

--- index.php
+++ index.php
@@ -1,7 +1,49 @@
-<form action="login.php?action=login" method="POST">
-    <label>Username : </label>
-    <input type="text" name="username" /><br />
-    <label>Password : </label>
-    <input type="text" name="password" /><br />
-    <input type="submit" name="submit" />
-</form>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Free Bootstrap Admin Template : Binary Admin</title>
+    <!-- BOOTSTRAP STYLES-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONTAWESOME STYLES-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- MORRIS CHART STYLES-->
+    <link href="assets/js/morris/morris-0.4.3.min.css" rel="stylesheet" />
+    <!-- CUSTOM STYLES-->
+    <link href="assets/css/custom.css" rel="stylesheet" />
+    <link href="assets/css/base.css" rel="stylesheet" />
+    <!-- GOOGLE FONTS-->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+<div id="wrapper">
+    <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0"></nav>
+    <div id="page-inner">
+        <div class="row">
+            <form class="col-lg-12" style="text-align: center" action="login.php?action=login" method="POST">
+                <label>Username : </label>
+                <input type="text" name="username" /><br />
+                <label>Password : </label>
+                <input type="text" name="password" /><br />
+                <input type="submit" name="submit" />
+            </form>
+
+        </div>
+        <!-- /. ROW  -->
+    </div>
+    <!-- /. PAGE INNER  -->
+</div>
+<!-- /. WRAPPER  -->
+<!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+<!-- JQUERY SCRIPTS -->
+<script src="assets/js/jquery-1.10.2.js"></script>
+<!-- BOOTSTRAP SCRIPTS -->
+<script src="assets/js/bootstrap.min.js"></script>
+<!-- METISMENU SCRIPTS -->
+<script src="assets/js/jquery.metisMenu.js"></script>
+<!-- CUSTOM SCRIPTS -->
+<script src="assets/js/custom.js"></script>
+
+</body>
+</html>
