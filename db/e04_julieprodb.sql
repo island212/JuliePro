a8053cb11c8e2b00883e23f08b4f76f4cc63a18a
IceSentry <c.giguere42@gmail.com>
2014-12-03 16:05:01 -0500

    Revert "Revert "Added admin to script and added personne_db""
    This reverts commit 44dcb314ef1ba574318bddbede02fc902346f327.

--- db/e04_julieprodb.sql
+++ db/e04_julieprodb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 03, 2014 at 09:47 PM
+-- Generation Time: Dec 03, 2014 at 09:58 PM
 -- Server version: 5.6.15-log
 -- PHP Version: 5.5.8
 
---
+++ model/personne_db.php
@@ -0,0 +1,21 @@
+<?php
+function get_type($username,$password){
+    global $db;
+    $query="SELECT type FROM Personne
+            WHERE username='$username',password='$password'";
+    $type=$db->query($query);
+    switch($type){
+        case 0:
+            return 'Super-utilisateur';
+            break;
+        case 1:
+            return 'Entraineur';
+            break;
+        case 2:
+            return 'Client';
+            break;
+    }
+}
+
+
+?>
