6859cda83ba4ad41ac00d99cb61a4d136308d4c2
ModoNoob <ModoNoob@hotmail.fr>
2014-12-03 15:28:06 -0500

    Basic db.php

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: 1381109
- * Date: 14-12-03
- * Time: 15:21
- */ 
+    $dbname = "e04_julieprodb";
+    $username = "";
+    $password = "";
+
+    $db = new PDO("mysql:host=localhost;dbname=$dbname", $username, $password);
+?>
