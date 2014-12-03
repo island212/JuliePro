67780d9440c3f88c3d7bb9528c6bf175228c4ee2
IceSentry <c.giguere42@gmail.com>
2014-12-03 16:23:12 -0500

    Added check for username and password in get_type()

--- model/personne_db.php
+++ model/personne_db.php
@@ -1,21 +1,38 @@
 <?php
-function get_type($username,$password){
+require('database.php');
+function get_type($username, $password)
+{
     global $db;
-    $query="SELECT type FROM Personne
-            WHERE username='$username',password='$password'";
-    $type=$db->query($query);
-    switch($type){
-        case 0:
-            return 'Super-utilisateur';
-            break;
-        case 1:
-            return 'Entraineur';
-            break;
-        case 2:
-            return 'Client';
-            break;
+    $usernameQuery = "SELECT username FROM Personne
+                    WHERE username='$username'";
+    $checkUsername = $db->query($usernameQuery);
+    if ($checkUsername == $username) {
+        $passwordQuery = "SELECT password FROM Personne
+        WHERE $password='$password'";
+        $checkPassword = $db->query($passwordQuery);
+        if ($checkPassword == $password) {
+            $query = "SELECT type FROM Personne
+                    WHERE username='$username',password='$password'";
+            $type = $db->query($query);
+            switch ($type) {
+                case 0:
+                    return "Super-utilisateur";
+                    break;
+                case 1:
+                    return "Entraineur";
+                    break;
+                case 2:
+                    return "Client";
+                    break;
+                default:
+                    return "type non-valide";
+                    break;
+            }
+        } else {
+            return "Le mot de passe n'est pas valide"
+        }
+    } else {
+        return "Le nom d'utilisateur n'existe pas";
     }
 }
-
-
 ?>
