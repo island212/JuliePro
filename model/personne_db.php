0313633d0dec6ccd43613abd3de7e0c25763d59a
IceSentry <c.giguere42@gmail.com>
2014-12-05 16:34:10 -0500

    added create_personne and get_idPersonne
    create_personne(username,password,firstName,lastName,type)
    get_idPersonne(username)

--- model/personne_db.php
+++ model/personne_db.php
@@ -21,4 +21,36 @@ function get_type($username, $password) {
         return -2;
     }
 }
+/*Retourne 0 et insert une personne si la requete fonctionne;
+  Retourne -1 si le username existe deja;*/
+function create_personne($username,$passowrd,$firstName,$lastName,$type){
+    global $db;
+    $usernameQuery = "SELECT username FROM personne
+                    WHERE username='$username'";
+    $checkUsername = $db->query($usernameQuery)->fetch();
+    if ($checkUsername[0] == $username) {
+        $insertQuery = "INSERT INTO personne (username,password,firstName,lastName,[type])
+                        VALUES ($$username,$$passowrd,$$firstName,$lastName,$type)";
+        $db->exec($insertQuery);
+        return 0;
+    } else {
+        return -1;
+    }
+}
+/*Retourne le idPersonne selon le username ou retourne -1 si le username n'existe pas*/
+function get_idPersonne($username){
+    global $db;
+    $usernameQuery = "SELECT username FROM personne
+                    WHERE username='$username'";
+    $checkUsername = $db->query($usernameQuery)->fetch();
+    if ($checkUsername[0] == $username) {
+        $selectQuery = "SELECT idPersonne FROM personne
+                        WHERE username='$username'";
+        $id=$db->query($selectQuery)->fetch();
+        return $id[0];
+    } else {
+        return -1;
+    }
+}
+
 ?>
