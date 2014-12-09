c62ee9ec06bb9b7304a99c799bbeea9fd83f7acc
IceSentry <c.giguere42@gmail.com>
2014-12-09 12:50:16 -0500

    form add_client

--- employee/client_add.php
+++ employee/client_add.php
@@ -5,23 +5,43 @@
             <form role="form" class="col-md-6">
                 <div class="form-group">
                     <label>Nom:</label>
-                    <input class="form-control">
+                    <input class="form-control" type="text" name="lastName" placeholder="Nom">
                 </div>
                 <div class="form-group">
                     <label>Prénom:</label>
-                    <input class="form-control">
+                    <input class="form-control" type="text" name="firstName" placeholder="Prenom">
                 </div>
                 <div class="form-group">
                     <label>Adresse:</label>
-                    <input class="form-control">
+                    <input class="form-control" type="text" name="adresse" placeholder="Adresse">
+                </div>
+                <div class="form-group">
+                    <label>Ville:</label>
+                    <input class="form-control" type="text" name="ville" placeholder="Ville">
+                </div>
+                <div class="form-group">
+                    <label>Code Postal:</label>
+                    <input class="form-control" type="text" pattern="[A-Za-z][0-9][A-Za-z] [0-9][A-Za-z][0-9]" name="cp" placeholder="HOH OHO">
+                </div>
+                <div class="form-group">
+                    <label>Telephone:</label>
+                    <input class="form-control" type="text" pattern="\[\(]d{3}[\)][\-]\d{3}[\-]\d{4}" name="telephone" placeholder="(123)-456-7890">
+                </div>
+                <div class="form-group">
+                    <label>Cellulaire:</label>
+                    <input class="form-control" type="text" pattern="\[\(]d{3}[\)][\-]\d{3}[\-]\d{4}" name="cellulaire" placeholder="(123)-456-7890">
                 </div>
                 <div class="form-group">
                     <label>Courriel:</label>
-                    <input class="form-control">
+                    <input class="form-control" type="email" name="courriel" placeholder="name@email.com">
                 </div>
                 <div class="form-group">
-                    <label>CodePostal:</label>
-                    <input class="form-control">
+                    <label>Date de naissance:</label>
+                    <input class="form-control" type="date" name="dateNaissance">
+                </div>
+                <div class="form-group">
+                    <label>Date d'inscription:</label>
+                    <input class="form-control" type="date" name="dateInscription">
                 </div>
             </form>
         </div>
