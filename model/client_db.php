2407102f5064337f72663db62260c812142e295f
IceSentry <c.giguere42@gmail.com>
2014-12-09 11:30:28 -0500

    indentation de marco modifier

--- model/client_db.php
+++ model/client_db.php
@@ -3,9 +3,8 @@
 function create_client($adresse, $cellulaire, $courriel, $cp, $dateInscription, $dateNaissance, $idPersonne, $telephone, $ville){
     global $db;
 
-    $query = "INSERT INTO `client`
-    (`telephone`, `cellulaire`, `adresse`, `ville`, `cp`, `dateNaissance`, `dateInscription`, `courriel`, `Personne_idPersonne`)
-    VALUES ('.$telephone.','.$cellulaire.','.$adresse.','.$ville.','.$cp.','.$dateNaissance.','.$dateInscription.','.$courriel.', '.$idPersonne.')";
+    $query = "INSERT INTO client (telephone, cellulaire, adresse, ville, cp, dateNaissance, dateInscription, courriel, Personne_idPersonne)
+              VALUES ('.$telephone.','.$cellulaire.','.$adresse.','.$ville.','.$cp.','.$dateNaissance.','.$dateInscription.','.$courriel.', '.$idPersonne.')";
 
     $db->query($query);
 }
