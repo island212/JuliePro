fc7bc07215714b9592e5a71196f81b29d7e440d3
ModoNoob <ModoNoob@hotmail.fr>
2014-12-12 23:14:31 -0500

    On peut maintenant se connecter à mon VPS
    Ça veut dire BD dans le Cloud Charles!!!

--- model/database.php
+++ model/database.php
@@ -1,10 +1,11 @@
 <?php
+    $hostname = "104.236.118.148:3306";
     $dbname = "e04_julieprodb";
-    $db_username = "mgs_user";
-    $db_password = "pa55word";
+    $db_username = "internal";
+    $db_password = "hunter2";
 
     try {
-        $db = new PDO("mysql:host=localhost;dbname=$dbname", $db_username, $db_password);
+        $db = new PDO("mysql:host=$hostname;dbname=$dbname", $db_username, $db_password);
     } catch(PDOException $e) {
         echo $e->getMessage();
     }
